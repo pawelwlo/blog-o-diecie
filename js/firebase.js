d44759fe46adf414524d32900d9b3fb3a871ef18
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-02-02 05:34:08 +0100

    latest
    new

--- js/firebase.js
+++ js/firebase.js
@@ -10,15 +10,11 @@
   const firebaseConfig = {
     apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
     authDomain: "diet-blog-b8ca5.firebaseapp.com",
-    databaseURL: "https://diet-blog-b8ca5-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "diet-blog-b8ca5",
     storageBucket: "diet-blog-b8ca5.appspot.com",
     messagingSenderId: "1086895413120",
-    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365",
-    measurementId: "G-DJXKKYQTW3"
+    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365"
   };
-
-
 
   // Use this to initialize the firebase App
   const app = initializeApp(firebaseConfig);
