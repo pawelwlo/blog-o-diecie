231427c4711ba18e6afc5c6929f89c4940e44a48
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-02-03 01:09:24 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -9,11 +9,21 @@
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
-    authDomain: "diet-blog-b8ca5.firebaseapp.com",
-    projectId: "diet-blog-b8ca5",
-    storageBucket: "diet-blog-b8ca5.appspot.com",
-    messagingSenderId: "1086895413120",
-    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365"
+
+  authDomain: "diet-blog-b8ca5.firebaseapp.com",
+
+  databaseURL: "https://diet-blog-b8ca5-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "diet-blog-b8ca5",
+
+  storageBucket: "diet-blog-b8ca5.appspot.com",
+
+  messagingSenderId: "1086895413120",
+
+  appId: "1:1086895413120:web:9159ecdebe13c4b29f9365",
+
+  measurementId: "G-DJXKKYQTW3"
+
   };
 
   // Use this to initialize the firebase App
